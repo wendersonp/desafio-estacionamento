82a79c9508811a6710068a076d58925b8323803e
Wenderson <wendersonpsg21@gmail.com>
2019-10-19 11:46:50 -0300

    Adicionado scheduling para liberar estacionamento e novos endpoints

--- src/main/java/com/conpaytest/desafioestacionamento/controller/Controller.java
+++ src/main/java/com/conpaytest/desafioestacionamento/controller/Controller.java
@@ -1,20 +1,25 @@
 package com.conpaytest.desafioestacionamento.controller;
 
-import com.conpaytest.desafioestacionamento.controller.exceptions.VagaOcupadaException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.Optional;
+
+import com.conpaytest.desafioestacionamento.controller.exceptions.ForbiddenException;
+import com.conpaytest.desafioestacionamento.controller.exceptions.NotFoundException;
 import com.conpaytest.desafioestacionamento.entities.Registro;
 import com.conpaytest.desafioestacionamento.entities.Vaga;
 import com.conpaytest.desafioestacionamento.repositories.RegistroRepository;
 import com.conpaytest.desafioestacionamento.repositories.VagaRepository;
 import com.conpaytest.desafioestacionamento.repositories.projections.PosicaoVagasDTO;
-import com.conpaytest.desafioestacionamento.repositories.projections.RelatorioDTO;
-
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.List;
-import java.util.Optional;
+import com.conpaytest.desafioestacionamento.repositories.projections.RelatorioByDataDTO;
+import com.conpaytest.desafioestacionamento.repositories.projections.RelatorioByVagaDTO;
+import com.conpaytest.desafioestacionamento.repositories.projections.RelatorioSumDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,11 +29,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller{
 
+
     @Autowired
     public VagaRepository vagaRepo;
 
     @Autowired
     public RegistroRepository registroRepo;
+
+    @Autowired
+    TaskScheduler taskScheduler; 
+
+    private class LiberarVagaRunnable implements Runnable{
+        private Vaga vaga;
+
+        LiberarVagaRunnable(Vaga vaga){
+            this.vaga = vaga;
+        }
+
+        public void run(){
+            vaga.setOcupada(false);
+            vagaRepo.save(vaga);
+        }
+    }
     
     @RequestMapping("")
     String test(){
@@ -43,8 +65,8 @@ public class Controller{
     @RequestMapping(value = "/vagas", method = RequestMethod.GET)
     ResponseEntity<List<PosicaoVagasDTO>> getVagas(){
         List<PosicaoVagasDTO> vagas = vagaRepo.findVagasLivres();
-        if(vagas.size() < 0){
-            return ResponseEntity.notFound().build();
+        if(vagas.size() == 0){
+            throw new NotFoundException("Nao Ha Vagas Disponiveis");
         }
         else{
             return ResponseEntity.accepted().body(vagas);
@@ -64,7 +86,7 @@ public class Controller{
         Vaga vaga = vagaOptional.get();
         
         if(vaga.isOcupada())
-            throw new VagaOcupadaException("Vaga esta ocupada");
+            throw new ForbiddenException("Vaga esta ocupada");
         
         LocalDateTime LocalDataEntrada;
         LocalDateTime LocalDataSaida;
@@ -104,46 +126,86 @@ public class Controller{
         return pendentes;
     }
 
-    @RequestMapping(value = "/pagamento/{idVaga}", method = RequestMethod.PUT)
-    ResponseEntity<Registro> pagar(
+    
+    @RequestMapping(value = "/saida/{idVaga}", method = RequestMethod.PUT)
+    Registro registrarSaida(
         @PathVariable("idVaga") String idVaga,
         @RequestParam(value = "data_saida", required = false) Integer dataSaida){
-        
         Registro reg = registroRepo.findByIdVaga(idVaga);
-        if(reg == null)
-            return ResponseEntity.notFound().build();
+        if(reg == null){
+            throw new NotFoundException("O registro para a vaga especificada nao foi encontrado");
+        }
+
         LocalDateTime dataDeSaida;
         if(dataSaida != null)
             dataDeSaida = LocalDateTime.ofEpochSecond(dataSaida.intValue(), 0, ZoneOffset.ofHours(-3));
         else
-            dataDeSaida = LocalDateTime.now();
-        
+            dataDeSaida = LocalDateTime.now().plusMinutes(5);
+
         if(reg.getDataDeEntrada().isBefore(dataDeSaida))
             reg.setDataDeSaida(dataDeSaida);
         else
-            reg.setDataDeSaida(LocalDateTime.now());
+            throw new ForbiddenException("A data de saida especificada deve ser apos a data de entrada");
 
         reg.setValorEstacionamento();
-            
+
+        registroRepo.save(reg);
+
+        return reg;
+    }
+
+    @RequestMapping(value = "/pagamento/{idVaga}", method = RequestMethod.PUT)
+    Registro pagar(
+        @PathVariable("idVaga") String idVaga){
+        
+        Registro reg = registroRepo.findByIdVaga(idVaga);
+        if(reg == null)
+            throw new NotFoundException("O registro especificado nao foi encontrado");
+
+        if(reg.getDataDeSaida() == null)
+            throw new ForbiddenException("O horário de saída do veículo ainda nao foi registrado");
         
         reg.setPago(true);
         Vaga vaga = vagaRepo.findById(reg.getVagaEstacionamento().getIdVaga()).get();
-        //TODO: Caso a data de saida seja no futuro, marcar um schedule para liberar estacionamento apenas no futuro
-        vaga.setOcupada(false);
+        if(LocalDateTime.now().isAfter(reg.getDataDeSaida())){
+            vaga.setOcupada(false);
+            vagaRepo.save(vaga);
+        }
+        else{
+            taskScheduler.schedule(new LiberarVagaRunnable(vaga), reg.getDataDeSaida().atZone(ZoneId.systemDefault()).toInstant());
+        }
 
         registroRepo.save(reg);
-        vagaRepo.save(vaga);
+        
 
-        return ResponseEntity.ok().body(reg);
+        return reg;
     }
 
     
+    @RequestMapping(value = "/relatorio/vaga", method = RequestMethod.GET)
+    List<RelatorioByVagaDTO> relatorioVaga(){
+        List<RelatorioByVagaDTO> rel = registroRepo.getRelatorioByVaga();
+        if(rel == null){
+            throw new NotFoundException("Nao foram encontrados registros");
+        }
+        return rel;
+    }
+
+    @RequestMapping(value = "/relatorio/data", method = RequestMethod.GET)
+    List<RelatorioByDataDTO> relatorioData(){
+        List<RelatorioByDataDTO> rel = registroRepo.getRelatorioByDate();
+        if(rel == null){
+            throw new NotFoundException("Nao foram encontrados registros");
+        }
+        return rel;
+    }
+
     @RequestMapping(value = "/relatorio", method = RequestMethod.GET)
-    RelatorioDTO relatorio(){
-        RelatorioDTO rel = new RelatorioDTO();
-        rel.byVagaDTO = registroRepo.getRelatorioByVaga();
-        rel.byDataDTO = registroRepo.getRelatorioByDate();
-        rel.sumDTO = registroRepo.getRelatorioSum();
-        return rel;   
+    RelatorioSumDTO relatorio(){
+        RelatorioSumDTO rel = registroRepo.getRelatorioSum();
+        if(rel == null){
+            throw new NotFoundException("Nao foram encontrados registros");
+        }
+        return rel;
     }
 }
--- src/main/java/com/conpaytest/desafioestacionamento/repositories/projections/RelatorioDTO.java
+++
@@ -1,9 +0,0 @@
-package com.conpaytest.desafioestacionamento.repositories.projections;
-
-import java.util.List;
-
-public class RelatorioDTO {
-    public List<RelatorioByDataDTO> byDataDTO;
-    public List<RelatorioByVagaDTO> byVagaDTO;
-    public RelatorioSumDTO sumDTO;
-}
